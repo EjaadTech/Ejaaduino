cfe69ac9a1ca1947b9cf4a5fc0bb2cf858627645
inamuj <nasirazizjumani@gmail.com>
2014-02-17 00:33:05 +0500

    Added Ejaaduino to top
    (Prepending instead of Appending)

--- CodeSandBox/EjaaduinoBoardAdder/EjaaduinoBoardAdder/Form1.cs
+++ CodeSandBox/EjaaduinoBoardAdder/EjaaduinoBoardAdder/Form1.cs
@@ -8,10 +8,12 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 
+
 namespace EjaaduinoBoardAdder
 {
     public partial class Form1 : Form
     {
+        char[] buffer = new char[2048];
         public Form1()
         {
             InitializeComponent();
@@ -25,17 +27,42 @@ namespace EjaaduinoBoardAdder
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 string path = openFileDialog1.FileName;
+                string tempFile = path + ".tmp"; //
+
+              //  File.Move(path, tempFile); //
+                File.Copy(path, tempFile);
+               // File.m
                 string sr = System.IO.File.ReadAllText(path);
                 {
                     if (sr.IndexOf("Ejaaduino") == -1)
                     {
-                        using (StreamWriter sw = File.AppendText(path))
+                        using (StreamReader reader = new StreamReader(tempFile))
                         {
-                            string ejaaduino = "\n###############################################################\nEjaaduino.name=Ejaaduino(ATmega8)\nEjaaduino.upload.protocol=usbasp\nEjaaduino.upload.maximum_size=6144\nEjaaduino.upload.speed=115200\nEjaaduino.upload.disable_flushing=true\nEjaaduino.bootloader.low_fuses=0x1F\nEjaaduino.bootloader.high_fuses=0xC0\nEjaaduino.build.mcu=atmega8\nEjaaduino.build.f_cpu=16000000L\nEjaaduino.build.core=arduino\nEjaaduino.build.variant=standard\n###############################################################";
-                            sw.WriteLine(ejaaduino);
-                        }	
+
+                            using (StreamWriter writer = new StreamWriter(path, false))
+                            {
+                                //  using (StreamWriter sw = File.AppendText(path))
+                                // {
+                                string ejaaduino = "\n###############################################################\nEjaaduino.name=Ejaaduino(ATmega8)\nEjaaduino.upload.protocol=usbasp\nEjaaduino.upload.maximum_size=6144\nEjaaduino.upload.speed=115200\nEjaaduino.upload.disable_flushing=true\nEjaaduino.bootloader.low_fuses=0x1F\nEjaaduino.bootloader.high_fuses=0xC0\nEjaaduino.build.mcu=atmega8\nEjaaduino.build.f_cpu=16000000L\nEjaaduino.build.core=arduino\nEjaaduino.build.variant=standard\n###############################################################";
+                                //  sw.WriteLine(ejaaduino);
+                                writer.Write(ejaaduino);
+
+                                int totalRead;
+
+                                while ((totalRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+
+                                    writer.Write(buffer, 0, totalRead);
+
+                                reader.Close();
+                            }
+
+
+                            
+                            File.Delete(tempFile);
+
+                        }
                     }
-                }   
+                }
             }
         }
     }
