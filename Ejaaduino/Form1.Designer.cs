d8a38f2a449ad6d12373dcb818ce3f477e4fec03
Zaid Pirwani <email@zaidpirwani.com>
2014-03-02 12:42:37 +0500

    Form Movement and Shadow/Border added
    form/window can now be moved, shadow added.
    
    Form size changed to reflect golden rectangle dimensions.

--- Ejaaduino/Form1.Designer.cs
+++ Ejaaduino/Form1.Designer.cs
@@ -29,19 +29,16 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btn_programmer = new System.Windows.Forms.Button();
             this.btn_usbSerial = new System.Windows.Forms.Button();
             this.btn_custom = new System.Windows.Forms.Button();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.btn_arduino = new System.Windows.Forms.Button();
             this.lnk_EjaadTech = new System.Windows.Forms.LinkLabel();
             this.lbl_heading = new System.Windows.Forms.Label();
             this.lbl_Exit = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_programmer
@@ -51,7 +48,7 @@
             this.btn_programmer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_programmer.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_programmer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_programmer.Location = new System.Drawing.Point(105, 139);
+            this.btn_programmer.Location = new System.Drawing.Point(66, 137);
             this.btn_programmer.Name = "btn_programmer";
             this.btn_programmer.Size = new System.Drawing.Size(133, 67);
             this.btn_programmer.TabIndex = 0;
@@ -65,7 +62,7 @@
             this.btn_usbSerial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_usbSerial.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_usbSerial.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_usbSerial.Location = new System.Drawing.Point(244, 139);
+            this.btn_usbSerial.Location = new System.Drawing.Point(205, 137);
             this.btn_usbSerial.Name = "btn_usbSerial";
             this.btn_usbSerial.Size = new System.Drawing.Size(133, 68);
             this.btn_usbSerial.TabIndex = 1;
@@ -79,30 +76,13 @@
             this.btn_custom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_custom.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_custom.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_custom.Location = new System.Drawing.Point(384, 139);
+            this.btn_custom.Location = new System.Drawing.Point(344, 137);
             this.btn_custom.Name = "btn_custom";
             this.btn_custom.Size = new System.Drawing.Size(133, 68);
             this.btn_custom.TabIndex = 2;
             this.btn_custom.Text = "Custom Firmware";
             this.btn_custom.UseVisualStyleBackColor = false;
             this.btn_custom.Click += new System.EventHandler(this.btn_custom_Click);
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 313);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(628, 22);
-            this.statusStrip1.SizingGrip = false;
-            this.statusStrip1.TabIndex = 3;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripStatusLabel1
-            // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
-            this.toolStripStatusLabel1.Text = "Status";
             // 
             // btn_arduino
             // 
@@ -111,9 +91,9 @@
             this.btn_arduino.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_arduino.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_arduino.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_arduino.Location = new System.Drawing.Point(105, 83);
+            this.btn_arduino.Location = new System.Drawing.Point(66, 82);
             this.btn_arduino.Name = "btn_arduino";
-            this.btn_arduino.Size = new System.Drawing.Size(412, 50);
+            this.btn_arduino.Size = new System.Drawing.Size(411, 48);
             this.btn_arduino.TabIndex = 4;
             this.btn_arduino.Text = "Open Arduino IDE";
             this.btn_arduino.UseVisualStyleBackColor = false;
@@ -122,7 +102,8 @@
             // lnk_EjaadTech
             // 
             this.lnk_EjaadTech.AutoSize = true;
-            this.lnk_EjaadTech.Location = new System.Drawing.Point(564, 318);
+            this.lnk_EjaadTech.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lnk_EjaadTech.Location = new System.Drawing.Point(467, 310);
             this.lnk_EjaadTech.Name = "lnk_EjaadTech";
             this.lnk_EjaadTech.Size = new System.Drawing.Size(59, 13);
             this.lnk_EjaadTech.TabIndex = 6;
@@ -133,7 +114,7 @@
             // lbl_heading
             // 
             this.lbl_heading.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_heading.Location = new System.Drawing.Point(106, 32);
+            this.lbl_heading.Location = new System.Drawing.Point(66, 31);
             this.lbl_heading.Name = "lbl_heading";
             this.lbl_heading.Size = new System.Drawing.Size(411, 48);
             this.lbl_heading.TabIndex = 7;
@@ -143,13 +124,13 @@
             // lbl_Exit
             // 
             this.lbl_Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbl_Exit.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Exit.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_Exit.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.lbl_Exit.Location = new System.Drawing.Point(600, -4);
+            this.lbl_Exit.Location = new System.Drawing.Point(512, 8);
             this.lbl_Exit.Name = "lbl_Exit";
-            this.lbl_Exit.Size = new System.Drawing.Size(28, 29);
+            this.lbl_Exit.Size = new System.Drawing.Size(23, 25);
             this.lbl_Exit.TabIndex = 8;
-            this.lbl_Exit.Text = "x";
+            this.lbl_Exit.Text = "X";
             this.lbl_Exit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lbl_Exit.Click += new System.EventHandler(this.lbl_Exit_Click);
             this.lbl_Exit.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_Exit_MouseDown);
@@ -158,9 +139,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(3, 212);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 212);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(623, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(518, 96);
             this.richTextBox1.TabIndex = 9;
             this.richTextBox1.Text = "";
             // 
@@ -170,42 +151,31 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // button1
-            // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(577, 184);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(45, 22);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "About";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(628, 335);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(542, 335);
             this.ControlBox = false;
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.lbl_Exit);
             this.Controls.Add(this.lbl_heading);
             this.Controls.Add(this.lnk_EjaadTech);
             this.Controls.Add(this.btn_arduino);
-            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.btn_custom);
             this.Controls.Add(this.btn_usbSerial);
             this.Controls.Add(this.btn_programmer);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Ejaaduino App V2.3";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
+            this.TransparencyKey = System.Drawing.Color.Lime;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -216,15 +186,12 @@
         private System.Windows.Forms.Button btn_programmer;
         private System.Windows.Forms.Button btn_usbSerial;
         private System.Windows.Forms.Button btn_custom;
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.Button btn_arduino;
         private System.Windows.Forms.LinkLabel lnk_EjaadTech;
         private System.Windows.Forms.Label lbl_heading;
         private System.Windows.Forms.Label lbl_Exit;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
--- Ejaaduino/Form1.cs
+++ Ejaaduino/Form1.cs
@@ -17,10 +17,24 @@ namespace Ejaaduino
         Process myProcess = new Process();
         public static string commandData = "";
 
+
+        //Form move Code - START
+        // from http://jachman.wordpress.com/2006/06/08/enhanced-drag-and-move-winforms-without-having-a-titlebar/
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if (message.Msg == WM_NCHITTEST && (int)message.Result == HTCLIENT)
+                message.Result = (IntPtr)HTCAPTION;
+        }
+        // Form Move code - END
+
         public static void getData(string data)
         {
             commandData+=data;
-
         }
         
         public Form1()
@@ -33,7 +47,7 @@ namespace Ejaaduino
         {
             this.Close();
         }
-        string v;
+
         private void lbl_Exit_MouseEnter(object sender, EventArgs e)
         {
             lbl_Exit.ForeColor = Color.White;
@@ -55,7 +69,7 @@ namespace Ejaaduino
         private void lnk_EjaadTech_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             // Specify that the link was visited. 
-            this.lnk_EjaadTech.LinkVisited = true;
+            this.lnk_EjaadTech.LinkVisited = false;
 
             // Navigate to a URL.
             System.Diagnostics.Process.Start("http://ejaad.com.pk/");
@@ -65,7 +79,7 @@ namespace Ejaaduino
         {
             Process p = new Process();
             p.StartInfo.FileName = Path.Combine(Application.StartupPath, "C:\\Program Files (x86)\\Arduino\\arduino.exe");
-           p.Start();
+            p.Start();
             StartProcess();
         }
 
@@ -73,21 +87,21 @@ namespace Ejaaduino
         {
             //Ejaaduino.Echo h = new Ejaaduino.Echo("Hello my 1st C# object !");
             //h.Tell();
-            string v="-c usbasp -p m8 -U flash:w:programmer.hex";
-      // System.Diagnostics.Process.Start("cmd.exe", "/k avrdude.exe -c usbasp -p m8 -U flash:w:programmer.hex");
-           // System.Diagnostics.myProcess.Start("avrdude.exe", "hey");
-       StartProcess();
-           // var processStartInfo = new ProcessStartInfo("cmd.exe");
+            //string v = "-c usbasp -p m8 -U flash:w:programmer.hex";
+            // System.Diagnostics.Process.Start("cmd.exe", "/k avrdude.exe -c usbasp -p m8 -U flash:w:programmer.hex");
+            // System.Diagnostics.myProcess.Start("avrdude.exe", "hey");
+            StartProcess();
+            // var processStartInfo = new ProcessStartInfo("cmd.exe");
             // Instantiate the writer
-          //  _writer = new TextBoxStreamWriter(txtConsole);
+            //  _writer = new TextBoxStreamWriter(txtConsole);
             // Redirect the out Console stream
-          //  Console.SetOut(_writer);
-      
-          //  Console.WriteLine("Now redirecting output to the text box");
-           // System.Diagnostics.Process.Start("");
+            //  Console.SetOut(_writer);
 
-      // string input = System.Console.ReadLine();
-       //richTextBox1.Text = input;
+            //  Console.WriteLine("Now redirecting output to the text box");
+            // System.Diagnostics.Process.Start("");
+
+            // string input      = System.Console.ReadLine();
+            //richTextBox1.Text = input;
         }
         private void StartProcess()
         {
@@ -111,8 +125,7 @@ namespace Ejaaduino
             }
         }
 
-        private static void ProcessOutputDataHandler(object sendingProcess,
-    DataReceivedEventArgs outLine)
+        private static void ProcessOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
         {
             if (!String.IsNullOrEmpty(outLine.Data))
             {
@@ -120,8 +133,7 @@ namespace Ejaaduino
             }
         }
 
-        private static void ProcessErrorDataHandler(object sendingProcess,
-DataReceivedEventArgs outLine)
+        private static void ProcessErrorDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
         {
             if (!String.IsNullOrEmpty(outLine.Data))
             {
@@ -146,26 +158,6 @@ DataReceivedEventArgs outLine)
            // Console.ReadLine(output);
            // richTextBox1.Text = output;
         }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-          //  Win32.FreeConsole(); // Free the console.
-        }
-
     }
-    //public class Win32
-   // {
-        /// <summary>
-        /// Allocates a new console for current process.
-        /// </summary>
-      //  [DllImport("kernel32.dll")]
-      //  public static extern Boolean AllocConsole();
-
-        /// <summary>
-        /// Frees the console.
-        /// </summary>
-     //   [DllImport("kernel32.dll")]
-      //  public static extern Boolean FreeConsole();
-  //  }
 }
 
