56f2c7a54d17c9c698737b09e496f101f07c8a44
inamuj <nasirazizjumani@gmail.com>
2014-03-20 02:33:52 +0500

    Included Exception Handling
    Crash landing to developer's inbox.

--- Ejaaduino/Program.cs
+++ Ejaaduino/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Threading;
+
 using System.Collections.Generic;
 using System.Windows.Forms;
+using CrashReporterDotNET;
+
 
 namespace Ejaaduino
 {
@@ -12,9 +16,39 @@ namespace Ejaaduino
         [STAThread]
         static void Main()
         {
+
+            Application.ThreadException += ApplicationThreadException;
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            ReportCrash((Exception)unhandledExceptionEventArgs.ExceptionObject);
+            Environment.Exit(0);
+        }
+
+        private static void ApplicationThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            ReportCrash(e.Exception);
+        }
+
+        private static void ReportCrash(Exception exception)
+        {
+            var reportCrash = new ReportCrash
+            {
+                FromEmail = "'nasirazizjumani@gmail.com",
+                ToEmail = "nasirazizjumani@gmail.com",
+                SmtpHost = "smtp.gmail.com",
+                Port = 587,
+                UserName = "nasirazizjumani@gmail.com",
+                Password = "klavikancube",
+                EnableSSL = true,
+            };
+
+            reportCrash.Send(exception);
+        }
     }
-}
+    }
