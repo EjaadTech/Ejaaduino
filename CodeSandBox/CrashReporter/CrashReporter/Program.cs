10c60e3f45ed71ba4161fc4988f3873367ba7a63
Zaid Pirwani <email@zaidpirwani.com>
2014-03-22 01:28:08 +0500

    CrashReporter checked in Sandbox
    CrashReporter.Net library tested for exception handling and sending of
    report back to us.
    
    from: http://crashreporterdotnet.codeplex.com
    
    requires email and password, hence another solution to be searched.

---
+++ CodeSandBox/CrashReporter/CrashReporter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using CrashReporterDotNET;
+
+namespace CrashReporter
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+
+            Application.ThreadException += ApplicationThreadException;
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            ReportCrash((Exception)unhandledExceptionEventArgs.ExceptionObject);
+            Environment.Exit(0);
+        }
+
+        private static void ApplicationThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            ReportCrash(e.Exception);
+        }
+
+        private static void ReportCrash(Exception exception)
+        {
+            var reportCrash = new ReportCrash
+            {
+                FromEmail = "Your Gmail address",
+                ToEmail = "Email address where you want to receive crash reports",
+                SmtpHost = "Your SMTP server address for example smtp.gmail.com",
+                Port = 587,
+                UserName = "Your Gmail address ",
+                Password = "Your Gmail password",
+                EnableSSL = true,
+            };
+
+            reportCrash.Send(exception);
+        }
+    }
+}
---
+++ CodeSandBox/CrashReporter/README.txt
@@ -0,0 +1,4 @@
+CrashReporter from http://crashreporterdotnet.codeplex.com
+
+
+WORKS..! but requires an email and a password to be put in source code, dont wanna do that..!
